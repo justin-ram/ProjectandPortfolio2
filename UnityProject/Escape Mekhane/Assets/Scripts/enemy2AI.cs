e9cd5ec17b371d8b82e1eb6eb796a183f5e1f31a
Vowrawn <cstefanese@gmail.com>
2026-08-08 22:19:07 -0400

    created enemy2 script

---
+++ UnityProject/Escape Mekhane/Assets/Scripts/enemy2AI.cs
@@ -0,0 +1,42 @@
+
+using UnityEngine;
+using System.Collections;
+
+public class enemy2AI : MonoBehaviour, IDamage
+{
+    [Range(1, 10)][SerializeField] int HP;
+    [SerializeField] Renderer model;
+    Color colorOrig;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        colorOrig = model.material.color;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void takeDamage(int amount)
+    {
+        HP -= amount;
+        if (HP <= 0)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            StartCoroutine(flashRed());
+        }
+    }
+    IEnumerator flashRed()
+    {
+        model.material.color = Color.red;
+        yield return new WaitForSeconds(0.1f);
+        model.material.color = colorOrig;
+    }
+    
+
+}

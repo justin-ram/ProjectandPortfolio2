c51689d128bdc808983f4af85014b98dce6354ab
ImmortalRaven <heydebrayden@gmail.com>
2026-08-09 14:29:37 -0400

    GAMEMANAGER YAY

---
+++ UnityProject/Escape Mekhane/Assets/Scripts/gameManager.cs
@@ -0,0 +1,62 @@
+using UnityEngine;
+//using UnityEngine.UI;
+
+public class gameManager : MonoBehaviour
+{
+    public static gameManager instance;
+
+    [SerializeField] GameObject menuActive;
+    [SerializeField] GameObject menuPause;
+    [SerializeField] GameObject menuWin;
+    [SerializeField] GameObject menuLose;
+
+    public bool isPaused;
+    public GameObject player;
+    public playerController playerScript;
+
+    float timeScaleOrig;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Awake()
+    {
+        instance = this;
+        player = GameObject.FindWithTag("Player");
+        playerScript = player.GetComponent<playerController>();
+        timeScaleOrig = Time.timeScale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Cancel"))
+        {
+            if (menuActive == null)
+            {
+                statePause(menuPause);
+            }
+            else if (menuActive == menuPause)
+            {
+                stateUnpause();
+            }
+        }
+    }
+
+    public void statePause(GameObject menu)
+    {
+        isPaused = true;
+        Time.timeScale = 0;
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+        menuActive = menu;
+        menuActive.SetActive(true);
+    }
+    public void stateUnpause()
+    {
+        isPaused = false;
+        Time.timeScale = timeScaleOrig;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        menuActive.SetActive(false);
+        menuActive = null;
+    }
+}

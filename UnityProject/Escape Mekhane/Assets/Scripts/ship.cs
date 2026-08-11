6e23598fb82ba13ca93ad8775d7d1d08c80bdc16
justin-ram <justinisaiahlewis@gmail.com>
2026-08-11 19:57:36 -0400

    player can now not hit himself. And ship script added but not complete.

---
+++ UnityProject/Escape Mekhane/Assets/Scripts/ship.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class ship : MonoBehaviour, IInteract
+{
+    public void Interact()
+    {
+        endGame();
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void endGame()
+    {
+        //if all ship parts are collected play win menu 
+    }
+    
+}

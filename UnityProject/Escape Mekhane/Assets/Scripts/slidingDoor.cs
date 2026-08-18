b5d15e0ceec75a8561707d4d9a63c6a2f37ef46c
ImmortalRaven <heydebrayden@gmail.com>
2026-08-18 15:58:56 -0400

    An interactable sliding door has been made

--- UnityProject/Escape Mekhane/Assets/Scripts/slidingDoor.cs
+++ UnityProject/Escape Mekhane/Assets/Scripts/slidingDoor.cs
@@ -1,16 +1,46 @@
 using UnityEngine;
+using System.Collections;
 
-public class SlidingDoor : MonoBehaviour
+public class SlidingDoor : MonoBehaviour, IInteract
 {
+    [SerializeField] GameObject pointB;
+    [SerializeField] int movementSpeed;
+
+    Vector3 startingPos;
+    Vector3 targetPos;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        startingPos = transform.position;
+        targetPos = pointB.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    IEnumerator moveDoor()
+    {
+        while ((targetPos - transform.position).sqrMagnitude > 0.01f)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, targetPos, movementSpeed * Time.deltaTime);
+            yield return null;
+        }
+
+        if (targetPos == startingPos)
+        {
+            targetPos = pointB.transform.position;
+        }
+        else if (targetPos == pointB.transform.position)
+        {
+            targetPos = startingPos;
+        }
+    }
+
+    public void Interact()
+    {
+        StartCoroutine(moveDoor());
     }
 }
